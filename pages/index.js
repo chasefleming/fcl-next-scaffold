8e70f6ab1e129b1aa737d327e395d5d6495b755e
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2022-10-26 13:47:15 -0700

    Change text and links

--- pages/index.js
+++ pages/index.js
@@ -13,42 +13,41 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <a href="https://developers.flow.com/tools/fcl-js">FCL</a> Next Scaffold
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          For the Flow Blockchain
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="https://developers.flow.com/tools/fcl-js/tutorials/flow-app-quickstart" className={styles.card}>
+            <h2>FCL Quickstart &rarr;</h2>
+            <p>Get up and running.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          <a href="https://developers.flow.com/tools/fcl-js/reference/api" className={styles.card}>
+            <h2>API Docs &rarr;</h2>
+            <p>Dig deeper into FCL.</p>
           </a>
 
           <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
+            href="https://developers.flow.com/tools/fcl-js/reference/discovery"
             className={styles.card}
           >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h2>Wallet Discovery &rarr;</h2>
+            <p>Integrate wallets.</p>
           </a>
 
           <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href=""
             target="_blank"
             rel="noopener noreferrer"
             className={styles.card}
           >
-            <h2>Deploy &rarr;</h2>
+            <h2>Scaffold Repo &rarr;</h2>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              View the repo.
             </p>
           </a>
         </div>
