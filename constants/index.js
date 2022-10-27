72d704ea9e91483029b9655c21e7d413e70d9017
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2022-10-27 14:42:11 -0700

    Setup config for each network

---
+++ constants/index.js
@@ -0,0 +1,5 @@
+export const ACCESS_NODE_URLS = {
+  'local': 'http://localhost:8080',
+  'testnet': 'https://rest-testnet.onflow.org',
+  'mainnet': 'https://rest-mainnet.onflow.org'
+}
--- flow/config.js
+++ flow/config.js
@@ -1,8 +1,14 @@
-import { config } from "@onflow/fcl"
+import { config } from '@onflow/fcl'
+import { ACCESS_NODE_URLS } from '../constants'
+
+const flowNetwork = process.env.NEXT_PUBLIC_FLOW_NETWORK
+
+console.log('Dapp running on network:', flowNetwork)
 
 config({
-  "accessNode.api": "http://localhost:8080", // Mainnet: "https://rest-mainnet.onflow.org"
-  "discovery.wallet": "https://fcl-discovery.onflow.org/local/authn", // Mainnet: "https://fcl-discovery.onflow.org/authn"
-  "app.detail.icon": "https://avatars.githubusercontent.com/u/62387156?v=4",
-  "app.detail.title": "FCL Next Scaffold"
+  'flow.network': flowNetwork,
+  'accessNode.api': ACCESS_NODE_URLS[flowNetwork],
+  'discovery.wallet': `https://fcl-discovery.onflow.org/${flowNetwork}/authn`,
+  'app.detail.icon': 'https://avatars.githubusercontent.com/u/62387156?v=4',
+  'app.detail.title': 'FCL Next Scaffold'
 })
