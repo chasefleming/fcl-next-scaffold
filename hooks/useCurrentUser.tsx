1525e0fe1fb69e8b30bce32f60735a0e5325046e
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2023-06-12 11:31:05 -0700

    Type useCurrentUser

--- hooks/useCurrentUser.tsx
+++ hooks/useCurrentUser.tsx
@@ -2,7 +2,7 @@ import * as fcl from '@onflow/fcl'
 import { CurrentUser } from '@onflow/typedefs'
 import { useEffect, useState } from 'react'
 
-export default function useCurrentUser() {
+export default function useCurrentUser(): CurrentUser {
   const [user, setUser] = useState<CurrentUser>({
     addr: null,
     cid: null,
