26c760ae5fc5eea3e02700fee99af795b3faa191
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2025-04-09 16:08:44 -0700

    Update layout

--- layouts/DefaultLayout.tsx
+++ layouts/DefaultLayout.tsx
@@ -1,10 +1,29 @@
-import Navbar from '../components/Navbar'
+import Navbar from "../components/Navbar";
+import { FlowProvider, FlowNetwork } from "@onflow/kit";
+import { ACCESS_NODE_URLS } from "../constants";
+import flowJSON from "../flow.json";
 
-export default function DefaultLayout({ children }) {
+const flowNetwork = process.env.NEXT_PUBLIC_FLOW_NETWORK as FlowNetwork;
+
+export default function DefaultLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <>
+    <FlowProvider
+      config={{
+        flowNetwork,
+        accessNodeUrl: ACCESS_NODE_URLS[flowNetwork],
+        appDetailTitle: "FCL Next Scaffold", // optional
+        appDetailIcon: "https://avatars.githubusercontent.com/u/62387156?v=4", // optional
+        appDetailUrl: "https://yourapp.com", // optional
+        appDetailDescription: "Your app description", // optional
+      }}
+      flowJson={flowJSON}
+    >
       <Navbar />
       <main>{children}</main>
-    </>
-  )
+    </FlowProvider>
+  );
 }
