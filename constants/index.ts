ec897cbd53cb81a1bf69bd75f50d6ff6eb049fdb
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2025-04-09 16:15:11 -0700

    Switch to emulator

--- constants/index.ts
+++ constants/index.ts
@@ -1,10 +1,10 @@
 export const ACCESS_NODE_URLS = {
-  'local': 'http://localhost:8888',
-  'testnet': 'https://rest-testnet.onflow.org',
-  'mainnet': 'https://rest-mainnet.onflow.org'
-}
+  emulator: "http://localhost:8888",
+  testnet: "https://rest-testnet.onflow.org",
+  mainnet: "https://rest-mainnet.onflow.org",
+};
 
 export const BLOCK_EXPLORER_URLS = {
-  'testnet': 'https://testnet.flowscan.org',
-  'mainnet': 'https://flowscan.org'
-}
+  testnet: "https://testnet.flowscan.org",
+  mainnet: "https://flowscan.org",
+};
