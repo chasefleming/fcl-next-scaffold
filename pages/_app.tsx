3ddbc8526549b5fa3dd18ce9e829914e2ab93433
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2025-04-10 09:29:02 -0700

    Convert

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,12 @@
-import '../styles/globals.css'
-import DefaultLayout from '../layouts/DefaultLayout'
-
-// Import FCL config
-import '../config/fcl'
+import "../styles/globals.css";
+import DefaultLayout from "../layouts/DefaultLayout";
 
 function MyApp({ Component, pageProps }) {
   return (
     <DefaultLayout>
       <Component {...pageProps} />
     </DefaultLayout>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
